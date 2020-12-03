6149902e255a7e4af2b9c8174e80702e50867f1f
zsevic <zsevic95@gmail.com>
2020-12-03 21:16:15 +0100

    Implement search bar

--- src/script.js
+++ src/script.js
@@ -10,6 +10,29 @@ const titleCase = (text) => text.split(' ').map(item => {
   return item.charAt(0) + item.substring(1).toLowerCase();
 }).join(' ');
 
+document.getElementById('input-search').addEventListener('input', (event) => {
+  const { value } = event.target;
+  if (!value) {
+    const countryList = document.querySelectorAll('.country');
+    for (const country of countryList.values()) {
+      country.parentElement.parentElement.style.display = '';
+    }
+    return;
+  }
+
+  const countryList = document.querySelectorAll('.country');
+  for (const country of countryList.values()) {
+    const [, ...name] = country.innerHTML.split(' ');
+    const countryName = name.join(' ').toLowerCase();
+
+    if (countryName.startsWith(value.toLowerCase())) {
+      country.parentElement.parentElement.style.display = '';
+    } else {
+      country.parentElement.parentElement.style.display = 'none';
+    }
+  }
+});
+
 const loadCountries = async () => {
   try {
     const API_URL = process.env.API_URL || 'https://open-borders.herokuapp.com';
@@ -25,7 +48,7 @@ const loadCountries = async () => {
       groups[type]?.forEach(({ name: countryName, info: countryInfo, flag }) => {
         const countryHtml = `<div class="list-group-item list-group-item-action flex-column align-items-start">
         <div class="d-flex w-100 justify-content-between">
-          <h5 class="mb-1 text-info">${flag} ${titleCase(countryName)}</h5>
+          <h5 class="mb-1 text-info country">${flag} ${titleCase(countryName)}</h5>
           </div>
           <p class="mb-1 text-muted" style="word-wrap:break-word">${countryInfo}</p></div>`;
         countriesByType.innerHTML += countryHtml;
