62d5d48ee29cceb8c065cc6f1cfa34881c514272
zsevic <zsevic95@gmail.com>
2020-12-26 14:59:46 +0100

    Update service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,15 +1,7 @@
-self.addEventListener('install', () => {
-  console.log('[ServiceWorker] Install');
+self.addEventListener('install', (event) => {
+  event.waitUntil(self.skipWaiting());
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches
-      .match(event.request, { ignoreSearch: true })
-      .then((response) => response || fetch(event.request)),
-  );
 });
