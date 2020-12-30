248de17fa3ce6bfb593c07d32d84c5af19238651
zsevic <zsevic95@gmail.com>
2020-12-30 02:16:08 +0100

    Fix service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -5,3 +5,5 @@ self.addEventListener('install', (event) => {
 self.addEventListener('activate', (event) => {
   event.waitUntil(self.clients.claim());
 });
+
+self.addEventListener('fetch', () => {});
