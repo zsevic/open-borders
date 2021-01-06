02108344ad4bc79e3635a49d5c89516e9474a728
zsevic <zsevic95@gmail.com>
2021-01-07 00:49:19 +0100

    Add theme color

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,6 +9,7 @@ class CustomDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
           <link rel="manifest" href="/manifest.json" />
+          <meta name="theme-color" content="#5bc0de" />
         </Head>
         <body>
           <Main />
