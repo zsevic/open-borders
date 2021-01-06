3ffd299feec7608e9e635122663fbec4718f516c
zsevic <zsevic95@gmail.com>
2021-01-07 00:04:43 +0100

    Improve logo width and height

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -9,8 +9,8 @@ export default function Logo() {
         <a style={{ cursor: 'pointer' }} id="logo">
           <Image
             src="/assets/logo-main.png"
-            width="190"
-            height="100"
+            width={190}
+            height={100}
             alt="logo"
           />
         </a>
