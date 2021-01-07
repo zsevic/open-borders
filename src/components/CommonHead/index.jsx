1ed15eb849b73568e20f2889227e7ff60bf6d95a
zsevic <zsevic95@gmail.com>
2021-01-07 19:51:06 +0100

    Add charset

--- src/components/CommonHead/index.jsx
+++ src/components/CommonHead/index.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 export default function CommonHead() {
   return (
     <Head>
+      <meta key="charset" charSet="utf-8" />
       <meta
         key="viewport"
         name="viewport"
