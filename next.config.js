d89737dfa7f7b576f8912c5e18f1294053638050
zsevic <zsevic95@gmail.com>
2021-01-11 21:42:26 +0100

    Improve caching

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
-const withWorkbox = require('next-with-workbox');
-
-module.exports = withWorkbox({
-  workbox: {
-    dest: 'public',
-    swDest: 'sw.js',
-    swSrc: 'src/services/service-worker.js',
-    force: false,
-  },
+module.exports = {
   src: './src',
-});
+};
